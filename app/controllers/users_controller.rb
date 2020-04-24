e4a5c7de32b8b8e2bf9e08f803dbfbe3769d4bb1
piecanfly88 <salchunr@gmail.com>
2020-04-24 00:59:03 -0400

    add post route for signup form to user controller and create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,14 @@ class UsersController < ApplicationController
     get "/signup" do
         erb :create_users
     end
+
+    post "/signup" do
+        @user = User.new(params)
+
+        if @user.save
+            session[:user_id] = @user.id
+        else
+            redirect to "/create_users"
+        end
+    end
 end
