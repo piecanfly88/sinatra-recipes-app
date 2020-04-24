9deff018b46c7a27b1cbf0994303851c396c4ea5
piecanfly88 <salchunr@gmail.com>
2020-04-23 21:10:47 -0400

    add user_id to recipes table

--- db/migrate/20200424004710_create_recipes.rb
+++ db/migrate/20200424004710_create_recipes.rb
@@ -4,6 +4,7 @@ class CreateRecipes < ActiveRecord::Migration
       t.string :title
       t.string :ingredients
       t.string :instructions
+      t.integer :user_id
 
       t.timestamps null: false
     end
