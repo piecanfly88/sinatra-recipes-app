5136ed20d00363ec6bd97ab6b4d96dced1792193
piecanfly88 <salchunr@gmail.com>
2020-04-28 15:04:41 -0400

    add login post route to users_controller with basic logic to log user in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,19 @@ class UsersController < ApplicationController
         end
     end
 
-    get '/login' do
+    get "/login" do
         erb :'users/login'
+    end
+
+    post "/login" do 
+        @user = User.find_by(username: params[:username])
+
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            
+            redirect "/users/#{@user.id}"
+        else
+            redirect '/login'
+        end
     end
 end
