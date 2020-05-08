f7bf84ddffa6bdabc8c99263bd889dd68b352f7f
piecanfly88 <salchunr@gmail.com>
2020-05-08 13:40:47 -0400

    add /recipes post route and create recipe from params then redirect to recipe show page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,5 +10,17 @@ class RecipesController < ApplicationController
         erb :'/recipes/create_recipe'
     end
 
+    post '/recipes' do
+        @recipe = Recipe.create(title: params[:title], ingredients: params[:ingredients], instructions: params[:instructions])
+
+        redirect "/recipes/#{@recipe.id}"
+    end
+    
+    get '/recipes/:id' do
+        @recipe = Recipe.find_by(id: params[:id])
+
+        erb :'/recipes/show'
+    end
+
     
 end
